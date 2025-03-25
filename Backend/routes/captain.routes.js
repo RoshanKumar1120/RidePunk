61f2466079e16e6f3085fc8f53694211891fa6b1
roshankumar <2023aspire77>
2025-03-25 15:44:26 +0530

    authorization for captain logout login complete

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controllers/captain.controller');
 const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
+const authmiddleware = require('../middlewares/auth.middleware');
 
 
 
@@ -17,8 +18,15 @@ router.post('/register', [
     captainController.registerCaptain
 )
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 
+],
+     captainController.loginCaptain
+)
 
-
+router.get('/profile', authmiddleware.authCaptain, captainController.getCaptainProfile)
+router.get('/logout', authmiddleware.authCaptain, captainController.logoutCaptain)
 
 module.exports = router;
