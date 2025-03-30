e25b12aaf7e8749fca82e46e47b47a77c26fe4a7
roshankumar <2023aspire77>
2025-03-30 16:06:46 +0530

    All signup,login page Complete for user and Captain

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import UserSignup from './pages/UserSignup'
 import UserLogin from './pages/UserLogin'
 import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
+import { UserDataContext } from './context/userContext'
 
 const App = () => {
+  
   return (
     <div>
       <Routes>
@@ -13,6 +16,8 @@ const App = () => {
         <Route path='/signup' element={<UserSignup/>}/>
         <Route path='/login' element={<UserLogin/>}/>
         <Route path='/captain-login' element={<Captainlogin/>}/>
+        <Route path='/captain-signup' element={<CaptainSignup/>}/>
+        
         
       </Routes>
     </div>
