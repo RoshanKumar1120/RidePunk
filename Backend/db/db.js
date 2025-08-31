b406ae3d0d28352fe6ccbb94ee8b566ac4e835c2
roshankumar <2023aspire77>
2025-09-01 00:19:46 +0530

    Db updated

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,13 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
+function connectToDb(){
+    mongoose.connect(process.env.DB_CONNECT,
+).then(()=>{
+    console.log('Connected to DB');
+}).catch(err=>console.log(err));
 
-function connectToDb() {
-  mongoose.connect(process.env.DB_CONNECT)
-    .then(() => {
-      console.log('✅ Connected to DB');
-    })
-    .catch((err) => {
-      console.error('❌ DB connection error:', err.message);
-    });
 }
 
-module.exports = connectToDb;
+module.exports=connectToDb
