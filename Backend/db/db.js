4e92716d8e0357fb3493b9bb6451c03e709b388a
roshankumar <2023aspire77>
2025-08-31 23:29:20 +0530

    api bug is fixed

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,10 +1,13 @@
-const mongoose=require('mongoose');
-function connectToDb(){
-    mongoose.connect(process.env.DB_CONNECT,
-).then(()=>{
-    console.log('Connected to DB');
-}).catch(err=>console.log(err));
+const mongoose = require('mongoose');
 
+function connectToDb() {
+  mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+      console.log('✅ Connected to DB');
+    })
+    .catch((err) => {
+      console.error('❌ DB connection error:', err.message);
+    });
 }
 
-module.exports=connectToDb
+module.exports = connectToDb;
